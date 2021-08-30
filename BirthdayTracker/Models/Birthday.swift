d1ed89cb4941477c5d06047d3323a7f61e2ad217
Olegajaro <88854903+Olegajaro@users.noreply.github.com>
2021-08-30 17:44:09 +0300

    Rename ViewController.swift (new title AddBirthdayViewController.swift)

--- BirthdayTracker/Models/Birthday.swift
+++ BirthdayTracker/Models/Birthday.swift
@@ -5,4 +5,14 @@
 //  Created by Олег Федоров on 30.08.2021.
 //
 
-import Foundation
+class Birthday {
+    let firstName: String
+    let lastName: String
+    let birthdate: Date
+    
+    init(firstName: String, lastName: String, birthDate: Date) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.birthdate = birthdate
+    }
+}
